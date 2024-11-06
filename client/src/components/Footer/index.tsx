43c8b6baaacb37e4447ff1635092a5e0d6e2197e
Ben Wright <bwright@2u.com>
2024-11-06 10:51:52 -0600

    Adds updated copyright

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
           >
             ❤️
           </span>{" "}
-          by the Word Guess team. &copy; 2024
+          by the Word Guess team. &copy; 2025
         </h4>
       </div>
     </footer>
